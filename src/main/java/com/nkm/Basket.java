3d9bcf3ad190628dd831f501b03e33894489db3f
nicholas.mcdowall <nicholas.mcdowall@asos.com>
2020-02-12 21:43:00 +0000

    failing test for multiple scenarios with no discount

--- src/main/java/com/nkm/Basket.java
+++ src/main/java/com/nkm/Basket.java
@@ -1,5 +1,7 @@
 package com.nkm;
 
+import java.time.MonthDay;
+
 public class Basket {
     public void add(int count, Item item) {
 
@@ -7,5 +9,9 @@ public class Basket {
 
     public double totalCost() {
         return 3.55;
+    }
+
+    public Basket with(int count, Item item) {
+        return this;
     }
 }
--- src/test/java/com/nkm/AcceptanceTests.java
+++ src/test/java/com/nkm/AcceptanceTests.java
@@ -1,10 +1,14 @@
 package com.nkm;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static java.time.LocalDate.now;
 import static java.time.Period.between;
@@ -30,14 +34,21 @@ public class AcceptanceTests {
         assertThat(pricingService.price(basket, now())).isEqualTo(3.15);
     }
 
-    @Test
-    void priceThreeSoupTinsAndTwoBreadLoavesBoughtTodayWithoutDiscount() {
+    @ParameterizedTest
+    @MethodSource("basketsNoDiscount")
+    void priceBasketWithoutDiscount(Basket basket, double expectedPrice) {
         pricingService = new PricingService();
 
-        basket.add(3, TIN_SOUP);
-        basket.add(2, BREAD_LOAF);
+        assertThat(pricingService.price(basket, now())).isEqualTo(expectedPrice);
+    }
 
-        assertThat(pricingService.price(basket, now())).isEqualTo(3.55);
+    private static Stream<Arguments> basketsNoDiscount() {
+        return Stream.of(
+                Arguments.of(new Basket(), 0),
+                Arguments.of(new Basket().with(1, BREAD_LOAF), 0.80),
+                Arguments.of(new Basket().with(1, TIN_SOUP), 0.65),
+                Arguments.of(new Basket().with(3, TIN_SOUP).with(2, BREAD_LOAF), 3.55)
+        );
     }
 
     private LocalDate yesterday() {
