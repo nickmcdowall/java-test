872a3b2f5ade22d0c8bd18208b561d73402bb6be
nicholas.mcdowall <nicholas.mcdowall@asos.com>
2020-02-14 21:09:21 +0000

    add additional input scenarios to test case for confidence

--- src/main/java/com/nkm/CommandPromptInteface.java
+++ src/main/java/com/nkm/CommandPromptInteface.java
@@ -6,9 +6,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.lang.String.format;
+import static java.util.regex.Pattern.compile;
 
 public class CommandPromptInteface {
 
+    public static final Pattern ADD_ITEM_PATTERN = compile("add ([0-9]+) ([A-Za-z]+)");
     public static final String GREETING = "Welcome to Henry’s Grocery! Use the following commands to purchase items.";
     public static final String GOODBYE = "Bye";
     public static final String INSTRUCTIONS =
@@ -16,15 +18,13 @@ public class CommandPromptInteface {
                     " - Type 'exit' to quit to application.\n" +
                     " - Type 'checkout' to get the final price.\n" +
                     " - Type 'restart' to empty the basket and start again.";
-    public static final String ADD_ITEM_REGEX = "add ([0-9]+) ([A-Za-z]+)";
-    public static final Pattern ADD_ITEM_PATTERN = Pattern.compile(ADD_ITEM_REGEX);
 
     public static void start(Scanner scanner, PrintStream out) {
         new CommandPromptInteface(scanner, out);
     }
 
     private final Scanner scanner;
-    private PrintStream out;
+    private final PrintStream out;
 
     private CommandPromptInteface(Scanner scanner, PrintStream out) {
         this.scanner = scanner;
--- src/test/java/com/nkm/CommandPromptIntefaceTest.java
+++ src/test/java/com/nkm/CommandPromptIntefaceTest.java
@@ -1,17 +1,23 @@
 package com.nkm;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import static com.nkm.CommandPromptInteface.*;
 import static java.lang.System.lineSeparator;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.joining;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class CommandPromptIntefaceTest {
 
@@ -28,19 +34,31 @@ class CommandPromptIntefaceTest {
         );
     }
 
-    @Test
-    void handleAddItemCommand() {
-        Scanner scanner = new Scanner(lines("add 1 Apple","exit"));
+    @ParameterizedTest
+    @MethodSource("variousValidAddCommands")
+    void handleAddItemCommand(String addCommand, List<String> expectedOutputLines) {
+        Scanner scanner = new Scanner(lines(addCommand, "exit"));
 
         CommandPromptInteface.start(scanner, new PrintStream(outputStream));
 
-        assertThat(outputStream.toString()).isEqualToIgnoringWhitespace(
-                lines(GREETING, INSTRUCTIONS,"+ 1 Apple added", GOODBYE)
+        assertThat(outputStream.toString()).contains(lines(expectedOutputLines));
+    }
+
+    private static Stream<Arguments> variousValidAddCommands() {
+        return Stream.of(
+                arguments("add 2 Apple", List.of("+ 2 Apple added")),
+                arguments("add 1 TinSoup add 1 TinSoup", List.of("+ 1 TinSoup added", "+ 1 TinSoup added")),
+                arguments("add 1 BreadLoaf add 1 BottleMilk", List.of("+ 1 BreadLoaf added", "+ 1 BottleMilk added")),
+                arguments("add 13 Apple add 0 TinSoup", List.of("+ 13 Apple added", "+ 0 TinSoup added"))
         );
     }
 
     private String lines(String... userInput) {
-        return asList(userInput).stream()
+        return lines(asList(userInput));
+    }
+
+    private String lines(List<String> userInput) {
+        return userInput.stream()
                 .collect(joining(lineSeparator()));
     }
 }
