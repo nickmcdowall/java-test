fcd37b09e907a798d65b6366f0027b4d40b6c4c2
nicholas.mcdowall <nicholas.mcdowall@asos.com>
2020-02-14 09:17:35 +0000

    add further scenarios for tin soup discount for confidence

--- src/test/java/com/nkm/AcceptanceTests.java
+++ src/test/java/com/nkm/AcceptanceTests.java
@@ -3,7 +3,8 @@ package com.nkm;
 import com.nkm.config.Config;
 import com.nkm.item.Item;
 import org.assertj.core.data.Offset;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static java.time.LocalDate.now;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,27 +18,20 @@ public class AcceptanceTests {
     private final Item tinSoup = appConfig.getTinSoup();
     private final Item breadLoaf = appConfig.getBreadLoaf();
 
-    @Test
-    void multiTinSoupOfferBoughtNow() {
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, +1, +2, +3, +4, +5})
+    void multiTinSoupOfferWhenBoughtNow(int offsetPurchaseDate) {
         application.addBasketItem(3, tinSoup);
         application.addBasketItem(2, breadLoaf);
-        assertThat(application.priceUp(now())).isEqualTo(3.15, ACCEPTABLE_OFFSET);
+        assertThat(application.priceUp(now().plusDays(offsetPurchaseDate))).isEqualTo(3.15, ACCEPTABLE_OFFSET);
     }
 
-    @Test
-    void multiTinSoupOfferBoughtBeforeDiscountApplies() {
+    @ParameterizedTest
+    @ValueSource(ints = {-2, +6})
+    void multiTinSoupOfferNotAppliedWhenBoughtOutsideDiscountWindow(int offsetPurchaseDate) {
         application.addBasketItem(3, tinSoup);
         application.addBasketItem(2, breadLoaf);
-        assertThat(application.priceUp(now().minusDays(2))).isEqualTo(3.55, ACCEPTABLE_OFFSET);
+        assertThat(application.priceUp(now().plusDays(offsetPurchaseDate))).isEqualTo(3.55, ACCEPTABLE_OFFSET);
     }
-
-    @Test
-    void multiTinSoupOfferBoughtAfterDiscountApplies() {
-        application.addBasketItem(3, tinSoup);
-        application.addBasketItem(2, breadLoaf);
-        assertThat(application.priceUp(now().plusDays(6))).isEqualTo(3.55, ACCEPTABLE_OFFSET);
-    }
-
-
 
 }
